eeea1c432627b5e3ad249d1e6005113868552139
Sjerrul <Sjerrul@users.noreply.github.com>
2022-02-24 08:50:02 +0000

    deploy: 2b0061fafaaa055e300d27c6a9fd92fefdfc593a

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Fastpages Notebook Blog Post",
+        "excerpt":"About This notebook is a demonstration of some of capabilities of fastpages with notebooks. With fastpages you can save your jupyter notebooks into the _notebooks folder at the root of your repository, and they will be automatically be converted to Jekyll compliant blog posts! Front Matter The first cell in...","categories": ["jupyter"],
+        "tags": [],
+        "url": "https://sjerrul.github.io/blog/jupyter/2020/02/20/test.html"
+      },{
+        "title": "A* Cost function experimentation",
+        "excerpt":"A* Cost Function Experimentation Introduction For several years, I have participated in the Advent Of Code, a series of challenges in the month of december. This year, on day 15, the challenge required navigating a grid where each step had a certain costs between 0 and 9. It allowed me...","categories": ["algorithms"],
+        "tags": [],
+        "url": "https://sjerrul.github.io/blog/algorithms/2021/12/15/a-star-cost-function-analysis.html"
+      }]
